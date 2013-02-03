5dacd06914ef7fe494498b1accf1d39134751921
Matej Urbas <matej.urbas@gmail.com>
2013-02-03 12:50:18 +0000

    Added a NetBeans project for TrainerJim.

--- backend/trainerjim/config/initializers/secret_token.rb
+++ backend/trainerjim/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Trainerjim::Application.config.secret_token = '2519fc74c33524ccf1dd5ba3d018c579be47b851b5eb81bcc44369258f9cd01dcb187352e56c92a9c637073e689a7899b090589765767ccf138af668a97c56d5'
+Trainerjim::Application.config.secret_token = 'a82bb9335ab118f47f2cf8e686de2e770d47e03a958f37bcbacd1c17e665e51d2243fea76368716687e77f5253ed2115f54bebdafea60d521c2e8e98e3a49709'
