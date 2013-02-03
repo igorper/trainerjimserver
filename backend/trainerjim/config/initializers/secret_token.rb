223bf4ffe561368cff0060370005362da0e04f7f
Matej Urbas <matej.urbas@gmail.com>
2013-02-03 10:37:09 +0000

    Created the backend project (the web server).

---
+++ backend/trainerjim/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trainerjim::Application.config.secret_token = '2519fc74c33524ccf1dd5ba3d018c579be47b851b5eb81bcc44369258f9cd01dcb187352e56c92a9c637073e689a7899b090589765767ccf138af668a97c56d5'
