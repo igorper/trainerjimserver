cb4477c3bde14891e6deecf9a7100b4025440fb3
Matej Urbas <matej.urbas@gmail.com>
2013-02-23 12:32:58 +0000

    Finished with the user table update.

--- db/migrate/20130223115507_merge_first_last_name.rb
+++ db/migrate/20130223115507_merge_first_last_name.rb
@@ -1,11 +1,6 @@
 class MergeFirstLastName < ActiveRecord::Migration
   def change
     add_column :users, :full_name, :string
-    Training.reset_column_information
-    Training.all.each { |t|
-      p.full_name = p.display_name
-      p.save
-    }
     remove_column :users, :first_name
     remove_column :users, :last_names
   end
