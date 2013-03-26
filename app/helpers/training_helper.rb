97f74d13db8a57d00a0bb54995f8b6646e9ea216
Matej Urbas <matej.urbas@gmail.com>
2013-03-26 18:00:56 +0100

    The exercise JSON now also includes the exercise type (with the name of the exercise).

--- app/helpers/training_helper.rb
+++ app/helpers/training_helper.rb
@@ -3,14 +3,16 @@ module TrainingHelper
   
   # Returns the JSON view filter for the Series model so as to include only
   # information required by the users (the browser).
-  @@series_view = { :only => [:id, :repeat_count, :weight, :rest_time] }
+  @@series_view = {:only => [:id, :repeat_count, :weight, :rest_time]}
   # A JSON field include filter that contains only fields relevant to the end user.
-  @@exercise_view = {:only => [:id, :name]}
+  @@exercise_view = {:only => [:id, :order]}
+  # A JSON field include filter that contains only fields relevant to the end user.
+  @@exercise_type_view = {:only => [:id, :name]}
   # A JSON field include filter that contains only fields relevant to the end user.
   @@training_view = {:only => [:id, :name]}
   # Returns the JSON view filter for the Exercise model so as to include only
   # information required by the users (the browser).
-  @@exercise_full_view = @@exercise_view.merge(:include => { :series => @@series_view })
+  @@exercise_full_view = @@exercise_view.merge(:include => { :series => @@series_view, :exercise_type => @@exercise_type_view })
   # Returns the JSON view filter for the Training model so as to include only
   # information required by the users (the browser).
   @@training_full_view = @@training_view.merge(:include => { :exercises => @@exercise_full_view })
