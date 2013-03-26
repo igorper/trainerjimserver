9dfcffb568b81fe24546c42ec513859885a11db7
Matej Urbas <matej.urbas@gmail.com>
2013-03-26 19:05:26 +0100

    Started with the Welcome page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,42 +13,45 @@ class DashboardController < ApplicationController
   end
   
   def exercisedates
-    
     #Group by month first, then by date
-    @measurements = Measurement.select("start_time, id,end_time").find_all_by_user_id(params[:user])
+    @measurements = Measurement
+    .select("start_time, id,end_time").find_all_by_user_id(params[:user])
     .group_by{|m| m.start_time.strftime("%B") }
-    .map{|k,v| {:month => k, :days => v.group_by{|m| m.start_time.day }.map{ |k,v| {:day => k, :measurements => v}}.sort_by{|k,v| v}.reverse}}
+    .map{|k,v| {:month => k, :days => v.group_by{|m| m.start_time.day }
+        .map{ |k,v| {:day => k, :measurements => v}}
+        .sort_by{|k,v| v}
+        .reverse}
+    }
     respond_to do |format|
-      format.json  { render :json => @measurements.to_json(
-        )   }
+      format.json  { render :json => @measurements.to_json() }
     end
   end
   
-    def exercise
-      respond_to do |format|
-        format.html
-        format.json{ 
-          render :json => SeriesExecution.where("exercise_id=? and measurement_id=?",params[:exercise],params[:measurement]).to_json
-        }       
-      end
+  def exercise
+    respond_to do |format|
+      format.html
+      format.json{ 
+        render :json => SeriesExecution.where("exercise_id=? and measurement_id=?",params[:exercise],params[:measurement]).to_json
+      }       
     end
+  end
   
   
-    def measurement
-      @measurement = Measurement.find_by_id(params[:id])
-      @exerciseTypes = SeriesExecution.where("measurement_id=?",params[:id]).group_by{|k| k.exercise.exercise_type.name }
-        .map{ |k,v| {:name => k, :executions => v}}
-      respond_to do |format|
-        format.html {render :layout => false}
-        format.json { render:json => {:types => @exerciseTypes, :measurement => @measurement.as_json(:include => [:measurement_comment])}}
-#        format.json  { render :json => @measurement.to_json(
-#            :include => [:series_executions]        
-#          )}
+  def measurement
+    @measurement = Measurement.find_by_id(params[:id])
+    @exerciseTypes = SeriesExecution.where("measurement_id=?",params[:id]).group_by{|k| k.exercise.exercise_type.name }
+    .map{ |k,v| {:name => k, :executions => v}}
+    respond_to do |format|
+      format.html {render :layout => false}
+      format.json { render:json => {:types => @exerciseTypes, :measurement => @measurement.as_json(:include => [:measurement_comment])}}
+      #        format.json  { render :json => @measurement.to_json(
+      #            :include => [:series_executions]        
+      #          )}
     
-      end
-  
-  
     end
   
   
   end
+  
+  
+end
