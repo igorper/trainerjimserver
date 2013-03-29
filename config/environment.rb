c232b73651bb37403b4844758948f4ce1f03fc62
Matej Urbas <matej.urbas@gmail.com>
2013-03-29 12:14:13 +0100

    Trying some more mail-sending stuff.

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,17 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Trainerjim::Application.initialize!
 
-Trainerjim::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :user_name => 'hello@trainerjim.com',
-    :password => 'Treniraj/Z/Jimom',
-    :domain => 'trainerjim.com',
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-end
+#Trainerjim::Application.configure do
+#  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.smtp_settings = {
+#    :address => 'smtp.gmail.com',
+#    :port => 587,
+#    :user_name => 'hello@trainerjim.com',
+#    :password => 'Treniraj/Z/Jimom',
+#    :domain => 'trainerjim.com',
+#    :authentication => :plain,
+#    :enable_starttls_auto => true
+#  }
+#end
 
 ActionMailer::Base.default :from => 'hello@trainerjim.com'
