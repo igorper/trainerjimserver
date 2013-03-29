7f1ea6405969a572f1f348a10dbd78c396d3a77c
Matej Urbas <matej.urbas@gmail.com>
2013-03-29 19:58:05 +0100

    Wrote a README file with instructions on how to deploy the trainerjim app on a fresh server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,15 +35,15 @@ Trainerjim::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :user_name => 'hello@trainerjim.com',
-    :password => 'Treniraj/Z/Jimom',
-    :domain => 'trainerjim.com',
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :sendmail
+#  config.action_mailer.smtp_settings = {
+#    :address => 'smtp.gmail.com',
+#    :port => 587,
+#    :user_name => 'hello@trainerjim.com',
+#    :password => 'Treniraj/Z/Jimom',
+#    :domain => 'trainerjim.com',
+#    :authentication => :plain,
+#    :enable_starttls_auto => true
+#  }
   config.action_mailer.default_url_options = { :host => 'localhost' }
 end
