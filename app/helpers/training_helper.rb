8eed0098a64f900bc02f2eb0ad156b8dcdc418a3
Matej Urbas <matej.urbas@gmail.com>
2013-03-31 20:02:15 +0200

    Organised the code a bit.

--- app/helpers/training_helper.rb
+++ app/helpers/training_helper.rb
@@ -1,18 +1,40 @@
 module TrainingHelper
-  mattr_reader :series_view, :exercise_view, :training_view, :exercise_full_view, :training_full_view
+  mattr_reader :series_view, :exercise_view, :training_view,
+    :exercise_full_view, :training_full_view, :exercise_type_view
+  
+  ###############################################################################
+  ### EXERCISE TYPE VIEWS
+  ##
+  
+  # A JSON field include filter that contains only fields relevant to the end user.
+  @@exercise_type_view = {:only => [:id, :name]}
+  
+  ###############################################################################
+  ### SERIES VIEWS
+  ##
   
   # Returns the JSON view filter for the Series model so as to include only
   # information required by the users (the browser).
   @@series_view = {:only => [:id, :repeat_count, :weight, :rest_time]}
+  
+  ###############################################################################
+  ### EXERCISE VIEWS
+  ##
+  
   # A JSON field include filter that contains only fields relevant to the end user.
   @@exercise_view = {:only => [:id, :order]}
-  # A JSON field include filter that contains only fields relevant to the end user.
-  @@exercise_type_view = {:only => [:id, :name]}
-  # A JSON field include filter that contains only fields relevant to the end user.
-  @@training_view = {:only => [:id, :name]}
+  
   # Returns the JSON view filter for the Exercise model so as to include only
   # information required by the users (the browser).
   @@exercise_full_view = @@exercise_view.merge(:include => { :series => @@series_view, :exercise_type => @@exercise_type_view })
+  
+  ###############################################################################
+  ### TRAINING VIEWS
+  ##
+  
+  # A JSON field include filter that contains only fields relevant to the end user.
+  @@training_view = {:only => [:id, :name]}
+  
   # Returns the JSON view filter for the Training model so as to include only
   # information required by the users (the browser).
   @@training_full_view = @@training_view.merge(:include => { :exercises => @@exercise_full_view })
