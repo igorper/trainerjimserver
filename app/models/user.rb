90a99f3053f4af985833872cc3e900ca97da8390
Matej Urbas <matej.urbas@gmail.com>
2013-03-31 17:16:28 +0200

    Still fixing our models. User does not have trainers or trainees.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,13 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :is_trainer
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :id, :email, :password, :password_confirmation, :remember_me,
-                  :admin, :full_name, :trainees, :trainer
+                  :admin, :full_name, :is_trainer
   
   has_many :measurements, :dependent => :delete_all
-  belongs_to :trainer, :class_name => "User", :foreign_key => "trainer_id"
-  has_many :trainees , :class_name => "User", :foreign_key => "trainer_id"
   
   @@RoleAdmin = 0b1
   
