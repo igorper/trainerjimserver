4a7d8af780af3f5cc0acd25b68c55bfea87ca0c5
Matej Urbas <matej.urbas@gmail.com>
2013-04-01 11:50:25 +0200

    Updated the application helper to allow using login/register partials in every controller.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  ##############################################################################
+  ### DEVISE (stuff for login/registration panels)
+  ##
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  ##
+  ### END: DEVISE (stuff for login/registration panels)
+  ##############################################################################
 end
