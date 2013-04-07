1b0d02948dfe91cc4d51d41811094f553760b085
Snuderl <Snuderl@gmail.com>
2013-04-07 18:36:14 +0200

    Added the ability to run rake tasks from capistrano. Ex: 'cap staging rake:invoke task=assets:precompile'

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,3 +32,12 @@ end
 # END: Passenger-specific stuff:
 
 require 'bundler/capistrano'
+
+
+namespace :rake do  
+  desc "Run a task on a remote server."  
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do  
+    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")  
+  end  
+end
