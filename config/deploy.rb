bff6a89e3901017eff0467d28dfb66f4446f6158
Matej Urbas <matej.urbas@gmail.com>
2013-07-05 02:02:49 +0100

    Created a new Capistrano task 'deploy:bootstrap', which should be called on the initial deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,6 @@ require 'rvm/capistrano'
 # END: RVM stuff
 
 
-after 'bundle:install', 'deploy:migrate'
-
 # START: Passenger-specific stuff:
 namespace :deploy do
   task :start do ; end
@@ -38,6 +36,26 @@ namespace :do do
   desc "Run a task on a remote server."  
   # run like: cap staging rake:invoke task=a_certain_task  
   task :invoke do  
-    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")  
+    run("cd #{deploy_to}/current; #{rake} RAILS_ENV=#{rails_env} #{ENV['task']}")
   end  
+end
+
+# START: We have to create the database first on cold deployment:
+namespace :deploy do
+  task :default do
+    update
+    migrate
+    restart
+  end
+  
+  task :bootstrap do
+    update
+    my_bootstrap
+    migrate
+    restart
+  end
+
+  task :my_bootstrap, :roles => :app do
+    run "cd #{current_path}; #{rake} RAILS_ENV=#{rails_env} db:create db:migrate db:bootstrap"
+  end
 end
