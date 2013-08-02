b261cb5d893a90c46cf1df86f794c5a813ec81de
Matej Urbas <matej.urbas@gmail.com>
2013-08-02 21:31:28 +0100

    Fixed the schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130716074450) do
+ActiveRecord::Schema.define(version: 20130719064923) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "conversations", force: true do |t|
     t.integer  "sender_id"
@@ -30,8 +33,8 @@ ActiveRecord::Schema.define(version: 20130716074450) do
   end
 
   create_table "exercises", force: true do |t|
-    t.integer  "training_id",      null: false
-    t.integer  "exercise_type_id", null: false
+    t.integer  "training_id",                                   null: false
+    t.integer  "exercise_type_id",                              null: false
     t.integer  "order"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -97,6 +100,8 @@ ActiveRecord::Schema.define(version: 20130716074450) do
     t.datetime "updated_at"
   end
 
+  add_index "newsletter_subscriptions", ["email"], name: "index_newsletter_subscriptions_on_email", unique: true, using: :btree
+
   create_table "roles", force: true do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
@@ -121,10 +126,6 @@ ActiveRecord::Schema.define(version: 20130716074450) do
     t.integer  "rest_time",    default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "duration_after_repetition"
-    t.integer  "duration_up_repetition"
-    t.integer  "duration_middle_repetition"
-    t.integer  "duration_down_repetition"
   end
 
   add_index "series", ["exercise_id"], name: "index_series_on_exercise_id", using: :btree
