0b785652a1cff9f5acb2427673f29e0624401686
Matej Urbas <matej.urbas@gmail.com>
2014-12-18 22:35:11 +0000

    Implemented login with angular.

--- app/assets/javascripts/userManagement.js
+++ app/assets/javascripts/userManagement.js
@@ -1,12 +1,13 @@
 //= require angular.min.js
 //= require api.js.erb
+//= require pageLinks.js.erb
 
 angular.module('userManagement', [])
   .directive('loginPanel', function () {
     return {
       restrict: 'E',
       scope: {},
-      controller: function ($scope, $http) {
+      controller: function ($scope, $http, $window) {
         $scope.isPanelVisible = false;
         $scope.isLoginActive = true;
 
@@ -20,12 +21,12 @@ angular.module('userManagement', [])
 
         $scope.loginSubmit = function () {
           $http
-            .post(api_v1_login_url)
+            .post(api_v1_login_url, {email: $scope.loginEmail, password: $scope.loginPassword})
             .success(function (data, status, headers) {
-              console.log("Successfully logged in.");
+              $window.location.href = workouts_url;
             })
             .error(function (data, status, headers) {
-              console.error("Could not log in.");
+              $window.alert("Could not log in. Check your email and password.")
             });
         };
       },
--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::LoginController < ActionController::Base
-  
+
   def login
-    render :json => '{"ohMy":"Hello, world!"}'
+    user_to_log_in = User.find_by_email(params[:email])
+    if user_to_log_in && user_to_log_in.valid_password?(params[:password])
+      sign_in(:user, user_to_log_in)
+      render :json => '{}'
+    else
+      render :json => '{}', :status => 404
+    end
   end
 
 end
