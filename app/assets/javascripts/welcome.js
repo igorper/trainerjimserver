e45606834ba84ab12a859ac724a0906e31a2f27e
Matej Urbas <matej.urbas@gmail.com>
2014-12-21 23:03:25 +0000

    Implemented the carousel with angular.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,13 +3,8 @@
 //= require ui-bootstrap-tpls-0.12.0.min.js
 //= require userManagement
 
-var welcomeApp = angular.module('welcome', ['userManagement']);
+var welcomeApp = angular.module('welcome', ['userManagement', 'ui.bootstrap']);
 
 welcomeApp.controller('WelcomeCtrl', function ($scope) {
-
-  $scope.onSignInButtonClicked = function (eventObject) {
-    var button = eventObject.target;
-    console.log(button);
-  }
 
 });
