c80c2e97d26179186b6b15a23d897f3ac4cdf344
Matej Urbas <matej.urbas@gmail.com>
2014-12-21 10:08:52 +0000

    Added Angular UI Bootstrap. Removed mailing list subscriptions from the welcome page.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,6 @@
 //= require apiLinks.js.erb
 //= require angular.min.js
+//= require ui-bootstrap-tpls-0.12.0.min.js
 //= require userManagement
 
 var welcomeApp = angular.module('welcome', ['userManagement']);
