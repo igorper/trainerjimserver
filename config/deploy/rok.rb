587a93fb73748c4c66272e01b475ffcb7097c9fa
Matej Urbas <matej.urbas@gmail.com>
2014-12-29 14:51:51 +0100

    Removed unused deployment configuration files.

--- config/deploy/rok.rb
+++
@@ -1,6 +0,0 @@
-role :web, "rok.trainerjim.com"
-role :app, "rok.trainerjim.com"
-role :db,  "rok.trainerjim.com", :primary => true
-
-set :rails_env, 'rok'
-set :repository, "git@bitbucket.org:rokstrnisa/trainerjimserver.git"
--- config/deploy/staging.rb
+++
@@ -1,5 +0,0 @@
-role :web, "dev.trainerjim.com"
-role :app, "dev.trainerjim.com"
-role :db,  "dev.trainerjim.com", :primary => true
-
-set :rails_env, 'staging'
