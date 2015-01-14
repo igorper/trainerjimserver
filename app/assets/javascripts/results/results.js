df38636327002f3b8572c16708268beae6bc1e80
Igor Pernek <igor@pernek.net>
2015-01-14 16:42:20 +0100

    Added basic visualizations of finished trainigs.

--- app/assets/javascripts/results/results.js
+++ app/assets/javascripts/results/results.js
@@ -1,13 +1,15 @@
 //= require apiLinks
 //= require angular-bootstrap/ui-bootstrap-tpls
 //= require shared/shared
+//= require measurements/measurement
 
 angular
   .module('results', [
     'ui.router',
     'ui.bootstrap',
     'shared',
-    'ui.calendar'
+    'ui.calendar',
+    'measurements'
   ])
   .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider) {
     $stateProvider
@@ -16,41 +18,50 @@ angular
         controller: "ResultsCtrl",
         templateUrl: "results/results.html"
       });
-  }]).factory("Result", ["$resource", function ($resource) {
-    return $resource("/api/v1/results/:id.json");
   }])
-  .controller("ResultsCtrl", ["$scope", "$http", "Result",
-    function($scope, $http, Result){
+  .controller("ResultsCtrl", ["$scope", "$http", "Measurement", '$compile','uiCalendarConfig',
+    function($scope, $http, Measurement, $compile, uiCalendarConfig){
 
-      var date = new Date();
-      var d = date.getDate();
-      var m = date.getMonth();
-      var y = date.getFullYear();
+      $scope.selectedTraining = null;
+      $scope.calendarSources = [];
 
-      $scope.eventSources = [];
-
-      $scope.results = null;
-      /* config object */
-      $scope.uiConfig = {
-        calendar:{
-          height: 450,
-          editable: true,
-          header:{
-            right: 'today prev,next'
-          },
-          dayClick: $scope.alertEventOnClick,
-          eventDrop: $scope.alertOnDrop,
-          eventResize: $scope.alertOnResize
-        }
-      };
-
-      Result.query(function (data) {
+      Measurement.query(function (data) {
         $scope.results = data;
 
-        $scope.eventSources.push([{start: new Date($scope.results[0].start_time), end: new Date($scope.results[0].end_time), title: "Workout"}])
+        for(var i=0; i < $scope.results.length; i++){
+          var r = $scope.results[i];
+          $scope.calendarSources.push([{start: new Date(r.start_time), end: new Date(r.end_time), title: r.id.toString(), training: r}])
+        }
+
       }, function (data, status, headers) {
         console.error("Could not fetch exercises.");
       });
+
+      $scope.alertOnEventClick = function( date, jsEvent, view){
+        $scope.selectedTraining = date;
+        uiCalendarConfig.calendars["myCalendar1"].fullCalendar("render");
+      };
+
+      /* Render Tooltip */
+      $scope.eventRender = function( event, element, view ) {
+        // TODO: tooltip
+        //element.attr({'tooltip': event.title,
+        //  'tooltip-append-to-body': true});
+        //$compile(element)($scope);
+
+        if($scope.selectedTraining && $scope.selectedTraining._id === event._id)
+          element.addClass("selectedTraining");
+      };
+      $scope.uiConfig = {
+        calendar:{
+          height: 550,
+          header:{
+            right: 'today prev,next'
+          },
+          eventClick: $scope.alertOnEventClick,
+          eventRender: $scope.eventRender
+        }
+      };
     }
   ])
 ;
