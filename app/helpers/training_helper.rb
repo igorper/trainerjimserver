326e1731c8760b5e07723f2cd22bc530cc3c47ca
Matej Urbas <matej.urbas@gmail.com>
2015-01-26 22:05:24 +0000

    Re-added Json views for trainings.

--- app/helpers/training_helper.rb
+++ app/helpers/training_helper.rb
@@ -1,4 +1,6 @@
 module TrainingHelper
+  mattr_reader :series_view, :exercise_view, :training_view,
+    :exercise_full_view, :training_full_view, :exercise_type_view
 
   def self.to_new_training(params)
     filtered_params = params.permit(:name)
@@ -8,6 +10,47 @@ module TrainingHelper
     end
     Training.new(filtered_params)
   end
+
+  ###############################################################################
+  ### EXERCISE TYPE VIEWS
+  ##
+
+  # A JSON field include filter that contains only fields relevant to the end user.
+  @@exercise_type_view = {:only => [:id, :name]}
+
+  ###############################################################################
+  ### SERIES VIEWS
+  ##
+
+  # Returns the JSON view filter for the Series model so as to include only
+  # information required by the users (the browser).
+  @@series_view = {:only => [:id, :repeat_count, :weight, :rest_time]}
+
+  ###############################################################################
+  ### EXERCISE VIEWS
+  ##
+
+  # A JSON field include filter that contains only fields relevant to the end user.
+  @@exercise_view = {:only => [:id, :order, :machine_setting, :duration_after_repetition, :duration_up_repetition, :duration_middle_repetition, :duration_down_repetition, :guidance_type]}
+
+  # Returns the JSON view filter for the Exercise model so as to include only
+  # information required by the users (the browser).
+  @@exercise_full_view = @@exercise_view.merge(:include => { :series => @@series_view, :exercise_type => @@exercise_type_view })
+
+  ###############################################################################
+  ### TRAINING VIEWS
+  ##
+
+  # A JSON field include filter that contains only fields relevant to the end user.
+  @@training_view = {:only => [:id, :name, :updated_at]}
+
+  # Returns the JSON view filter for the Training model so as to include only
+  # information required by the users (the browser).
+  @@training_full_view = @@training_view.merge(:include => { :exercises => @@exercise_full_view })
+
+  ###############################################################################
+  ### HELPER METHODS
+  ##
 
   # Saves the given training (in JSON form) as the user's new training.
   # This method also assigns the original training to the new training, if the
