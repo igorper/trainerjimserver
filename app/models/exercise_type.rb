9274c3895b5e6e98ea26c44e1c5f0a4e2a113d7f
Matej Urbas <matej.urbas@gmail.com>
2015-05-16 17:41:07 +0100

    Added images to exercise types.

--- app/models/exercise_type.rb
+++ app/models/exercise_type.rb
@@ -2,4 +2,16 @@ class ExerciseType < ActiveRecord::Base
   has_attached_file :image, styles: { large: "1400x900>", medium: "600x600>", thumb: "128x128>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+
+  def thumb_image_url
+    image.url(:thumb)
+  end
+
+  def medium_image_url
+    image.url(:medium)
+  end
+
+  def large_image_url
+    image.url(:large)
+  end
 end
