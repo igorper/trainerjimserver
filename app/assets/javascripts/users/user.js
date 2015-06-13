36eb4a9a6ba908326351257c25e37093c1fd97ea
Matej Urbas <matej.urbas@gmail.com>
2015-06-13 20:07:02 +0100

    Spacing!

--- app/assets/javascripts/users/user.js
+++ app/assets/javascripts/users/user.js
@@ -19,11 +19,14 @@ users.factory('User', ['$resource', 'currentUserChanged', function ($resource, c
     setNameImpl: {method: 'POST', url: '/api/v1/users/:id/name.json'},
     setPasswordImpl: {method: 'POST', url: '/api/v1/users/:id/password.json'}
   });
+
   User.setName = function (params, successCallback, failureCallback) {
     return User.setNameImpl(params, currentUserChanged(successCallback), failureCallback)
   };
+
   User.setPassword = function (params, successCallback, failureCallback) {
     return User.setPasswordImpl(params, currentUserChanged(successCallback), failureCallback)
   };
+
   return User;
 }]);
