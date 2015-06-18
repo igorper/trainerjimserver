9d3824d2aedd1a087447d32e0d8fde31e9f5893c
Matej Urbas <matej.urbas@gmail.com>
2015-06-18 08:23:15 +0100

    Removed the image from exercise types.

--- app/models/exercise_type.rb
+++ app/models/exercise_type.rb
@@ -1,7 +1,4 @@
 class ExerciseType < ActiveRecord::Base
-  has_attached_file :image, styles: { large: "1400x900>", medium: "600x600>", thumb: "128x128>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
   has_many :user_exercise_photos
   has_many :exercises
 end
