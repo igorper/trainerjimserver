847855f6cbe59b57b6d9e6af150cd12d89395e4c
Matej Urbas <matej.urbas@gmail.com>
2015-08-03 10:16:28 +0100

    Including the timestamp in the etag.

--- app/controllers/api/v1/exercise_groups_controller.rb
+++ app/controllers/api/v1/exercise_groups_controller.rb
@@ -2,8 +2,9 @@ class Api::V1::ExerciseGroupsController < ActionController::Base
 
   def index
     @exercise_groups = ExerciseGroup.all
-    fresh_when last_modified: @exercise_groups.maximum(:updated_at).try(:utc),
-               etag: "exercise_groups:index",
+    exercise_groups_latest_timestamp = @exercise_groups.maximum(:updated_at).try(:utc)
+    fresh_when last_modified: exercise_groups_latest_timestamp,
+               etag: "exercise_groups:index;timestamp:#{exercise_groups_latest_timestamp}",
                template: false
   end
 
