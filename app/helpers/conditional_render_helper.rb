77311f48343f89666ea5c98e80e950e0966019d4
Matej Urbas <matej.urbas@gmail.com>
2015-08-04 18:41:23 +0100

    Editing existing exercise types now works.

--- app/helpers/conditional_render_helper.rb
+++ app/helpers/conditional_render_helper.rb
@@ -3,7 +3,7 @@ module ConditionalRenderHelper
   def conditional_render(resource_list, resource_name)
     latest_timestamp = resource_list.maximum(:updated_at).try(:utc)
     fresh_when last_modified: latest_timestamp,
-               etag: "#{resource_name};timestamp:#{latest_timestamp}",
+               etag: "#{resource_name};timestamp:#{latest_timestamp};count:#{resource_list.count}",
                template: false
   end
 
