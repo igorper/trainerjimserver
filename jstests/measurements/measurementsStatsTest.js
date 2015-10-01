02c0391a5beeb72014e993bab79d833a62aa6c36
Matej Urbas <matej.urbas@gmail.com>
2015-10-01 18:07:37 +0100

    Simplified test harness.

--- jstests/measurements/measurementsStatsTest.js
+++ jstests/measurements/measurementsStatsTest.js
@@ -1,8 +1,7 @@
 describe("Measurements Stats", function () {
-  var MeasurementStats;
   beforeEach(module('measurements.stats'));
-  beforeEach(inject(['MeasurementStats', function (_MeasurementStats) {
-    MeasurementStats = _MeasurementStats;
+  beforeEach(inject(['MeasurementStats', function (MeasurementStats) {
+    this.MeasurementStats = MeasurementStats;
   }]));
 
   it("determines that there are no skipped series", function () {
@@ -10,8 +9,8 @@ describe("Measurements Stats", function () {
       "training": {"exercises": [{"series": [{"id": 1}, {"id": 2}], "exercise_type": {}}]},
       "series_executions": [{"series_id": 1}, {"series_id": 2}]
     };
-    var stats = MeasurementStats.calculateMeasurementStats(measurements);
-    expect(stats.seriesSkipped).toBe(0);
+    var stats = this.MeasurementStats.calculateMeasurementStats(measurements);
+    expect(stats.seriesSkipped).toEqual(0);
   });
 
   it("calculates skipped series when series executions are duplicated", function () {
@@ -19,7 +18,7 @@ describe("Measurements Stats", function () {
       "training": {"exercises": [{"series": [{"id": 1}, {"id": 2}], "exercise_type": {}}]},
       "series_executions": [{"series_id": 1}, {"series_id": 1}]
     };
-    var stats = MeasurementStats.calculateMeasurementStats(measurements);
-    expect(stats.seriesSkipped).toBe(1);
+    var stats = this.MeasurementStats.calculateMeasurementStats(measurements);
+    expect(stats.seriesSkipped).toEqual(1);
   });
 });
